5f5b05f58db52e21dfb79426f06b0425850287a6
Jonas Witschel <git@diabonas.de>
2021-12-16 15:00:28 +0100

    Add day 6

---
+++ 06/part1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# SPDX-License-Identifier: MIT
+
+import numpy as np
+
+NUMBER_OF_DAYS = 80
+
+with open("input", "r") as file:
+    initial_state = list(map(int, file.readline().split(",")))
+
+initial_ages = np.array([initial_state.count(age) for age in range(9)])
+
+TRANSITION_MATRIX = np.array(
+    [
+        [0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+)
+
+final_ages = np.linalg.matrix_power(TRANSITION_MATRIX, NUMBER_OF_DAYS).dot(initial_ages)
+sum_lanternfish = sum(final_ages)
+
+print(f"number of lanternfish after {NUMBER_OF_DAYS} days: {sum_lanternfish}")
