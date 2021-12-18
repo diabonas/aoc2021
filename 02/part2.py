diabonas/aoc2021

commit 7d3afc34807e9d309cd7d4fd5f5fd8e880925e42

Use Python 3.10 match-case statements where applicable

02/part2.py
@@ -11,14 +11,15 @@ horizontal = 0
 depth = 0
 aim = 0
 for c in commands:
-    if c["direction"] == "forward":
-        horizontal += c["distance"]
-        depth += aim * c["distance"]
-    elif c["direction"] == "down":
-        aim += c["distance"]
-    elif c["direction"] == "up":
-        aim -= c["distance"]
-    else:
-        raise Exception("invalid direction", c)
+    match c["direction"]:
+        case "forward":
+            horizontal += c["distance"]
+            depth += aim * c["distance"]
+        case "down":
+            aim += c["distance"]
+        case "up":
+            c["distance"]
+        case _:
+            raise Exception("invalid direction", c)
 
 print("product of final horizontal position and depth: %i" % (horizontal * depth))

13/part2.py
@@ -31,20 +31,21 @@ for dot in dots:
     paper[dot[1], dot[0]] = True
 
 for instruction in instructions:
-    if instruction["direction"] == "x":
-        assert not np.any(paper[:, instruction["line"]])
+    match instruction["direction"]:
+        case "x":
+            assert not np.any(paper[:, instruction["line"]])
 
-        # The instructions always seem to fold the paper in half, otherwise some
-        # padding would need to be applied to the smaller side
-        assert instruction["line"] == np.shape(paper)[1] // 2
+            # The instructions always seem to fold the paper in half, otherwise some
+            # padding would need to be applied to the smaller side
+            assert instruction["line"] == np.shape(paper)[1] // 2
 
-        paper = paper[:, : instruction["line"]] | paper[:, : instruction["line"] : -1]
-    elif instruction["direction"] == "y":
-        assert not np.any(paper[instruction["line"], :])
-        assert instruction["line"] == np.shape(paper)[0] // 2
-        paper = paper[: instruction["line"], :] | paper[: instruction["line"] : -1, :]
-    else:
-        raise Exception("invalid direction", instruction["direction"])
+            paper = paper[:, : instruction["line"]] | paper[:, : instruction["line"] : -1]
+        case "y":
+            assert not np.any(paper[instruction["line"], :])
+            assert instruction["line"] == np.shape(paper)[0] // 2
+            paper = paper[: instruction["line"], :] | paper[: instruction["line"] : -1, :]
+        case _:
+            raise Exception("invalid direction", instruction["direction"])
 
 print("folded paper:")
 for row in paper:

16/part2.py
@@ -23,61 +23,63 @@ def parse_packets(message):
         packet["value"] = int(value, 2)
     else:  # operator packet
         length_type_id = message[6]
-        if length_type_id == "0":
-            HEADER_LENGTH = 3 + 3 + 1 + 15  # version, type ID, length type ID, length
-            total_subpacket_length = int(message[7:HEADER_LENGTH], 2)
-            packet["length"] = HEADER_LENGTH
-            while packet["length"] != HEADER_LENGTH + total_subpacket_length:
-                subpacket = parse_packets(message[packet["length"] :])
-                packet["subpackets"].append(subpacket)
-                packet["length"] += subpacket["length"]
-        elif length_type_id == "1":
-            HEADER_LENGTH = 3 + 3 + 1 + 11  # version type ID, length type ID, length
-            num_subpackets = int(message[7:HEADER_LENGTH], 2)
-            packet["length"] = HEADER_LENGTH
-            for s in range(num_subpackets):
-                subpacket = parse_packets(message[packet["length"] :])
-                packet["subpackets"].append(subpacket)
-                packet["length"] += subpacket["length"]
-        else:
-            raise Exception("unknown length type ID", length_type_id)
+        match length_type_id:
+            case "0":
+                HEADER_LENGTH = 3 + 3 + 1 + 15  # version, type ID, length type ID, length
+                total_subpacket_length = int(message[7:HEADER_LENGTH], 2)
+                packet["length"] = HEADER_LENGTH
+                while packet["length"] != HEADER_LENGTH + total_subpacket_length:
+                    subpacket = parse_packets(message[packet["length"] :])
+                    packet["subpackets"].append(subpacket)
+                    packet["length"] += subpacket["length"]
+            case "1":
+                HEADER_LENGTH = 3 + 3 + 1 + 11  # version type ID, length type ID, length
+                num_subpackets = int(message[7:HEADER_LENGTH], 2)
+                packet["length"] = HEADER_LENGTH
+                for s in range(num_subpackets):
+                    subpacket = parse_packets(message[packet["length"] :])
+                    packet["subpackets"].append(subpacket)
+                    packet["length"] += subpacket["length"]
+            case _:
+                raise Exception("unknown length type ID", length_type_id)
 
     return packet
 
 
 def evaluate_packet(packet):
-    if packet["type"] == 0:
-        assert len(packet["subpackets"]) > 0
-        return sum(evaluate_packet(p) for p in packet["subpackets"])
-    elif packet["type"] == 1:
-        assert len(packet["subpackets"]) > 0
-        return prod(evaluate_packet(p) for p in packet["subpackets"])
-    elif packet["type"] == 2:
-        assert len(packet["subpackets"]) > 0
-        return min(evaluate_packet(p) for p in packet["subpackets"])
-    elif packet["type"] == 3:
-        assert len(packet["subpackets"]) > 0
-        return max(evaluate_packet(p) for p in packet["subpackets"])
-    elif packet["type"] == 4:
-        assert len(packet["subpackets"]) == 0
-        return packet["value"]
-    elif packet["type"] == 5:
-        assert len(packet["subpackets"]) == 2
-        return evaluate_packet(packet["subpackets"][0]) > evaluate_packet(
-            packet["subpackets"][1]
-        )
-    elif packet["type"] == 6:
-        assert len(packet["subpackets"]) == 2
-        return evaluate_packet(packet["subpackets"][0]) < evaluate_packet(
-            packet["subpackets"][1]
-        )
-    elif packet["type"] == 7:
-        assert len(packet["subpackets"]) == 2
-        return evaluate_packet(packet["subpackets"][0]) == evaluate_packet(
-            packet["subpackets"][1]
-        )
-    else:
-        raise Exception("unknown type ID", packet["type"])
+    match packet["type"]:
+        case 0:
+            assert len(packet["subpackets"]) > 0
+            return sum(evaluate_packet(p) for p in packet["subpackets"])
+        case 1:
+            assert len(packet["subpackets"]) > 0
+            return prod(evaluate_packet(p) for p in packet["subpackets"])
+        case 2:
+            assert len(packet["subpackets"]) > 0
+            return min(evaluate_packet(p) for p in packet["subpackets"])
+        case 3:
+            assert len(packet["subpackets"]) > 0
+            return max(evaluate_packet(p) for p in packet["subpackets"])
+        case 4:
+            assert len(packet["subpackets"]) == 0
+            return packet["value"]
+        case 5:
+            assert len(packet["subpackets"]) == 2
+            return evaluate_packet(packet["subpackets"][0]) > evaluate_packet(
+                packet["subpackets"][1]
+            )
+        case 6:
+            assert len(packet["subpackets"]) == 2
+            return evaluate_packet(packet["subpackets"][0]) < evaluate_packet(
+                packet["subpackets"][1]
+            )
+        case 7:
+            assert len(packet["subpackets"]) == 2
+            return evaluate_packet(packet["subpackets"][0]) == evaluate_packet(
+                packet["subpackets"][1]
+            )
+        case _:
+            raise Exception("unknown type ID", packet["type"])
 
 
 with open("input", "r") as file:

02/part1.py
@@ -10,13 +10,14 @@ with open("input", "r") as file:
 horizontal = 0
 depth = 0
 for c in commands:
-    if c["direction"] == "forward":
-        horizontal += c["distance"]
-    elif c["direction"] == "down":
-        depth += c["distance"]
-    elif c["direction"] == "up":
-        depth -= c["distance"]
-    else:
-        raise Exception("invalid direction", c)
+    match c["direction"]:
+        case "forward":
+            horizontal += c["distance"]
+        case "down":
+            depth += c["distance"]
+        case "up":
+            depth -= c["distance"]
+        case _:
+            raise Exception("invalid direction", c)
 
 print("product of final horizontal position and depth: %i" % (horizontal * depth))

13/part1.py
@@ -31,19 +31,20 @@ for dot in dots:
     paper[dot[1], dot[0]] = True
 
 instruction = instructions[0]
-if instruction["direction"] == "x":
-    assert not np.any(paper[:, instruction["line"]])
+match instruction["direction"]:
+    case "x":
+        assert not np.any(paper[:, instruction["line"]])
 
-    # The instructions always seem to fold the paper in half, otherwise some
-    # padding would need to be applied to the smaller side
-    assert instruction["line"] == np.shape(paper)[1] // 2
+        # The instructions always seem to fold the paper in half, otherwise some
+        # padding would need to be applied to the smaller side
+        assert instruction["line"] == np.shape(paper)[1] // 2
 
-    paper = paper[:, : instruction["line"]] | paper[:, : instruction["line"] : -1]
-elif instruction["direction"] == "y":
-    assert not np.any(paper[instruction["line"], :])
-    assert instruction["line"] == np.shape(paper)[0] // 2
-    paper = paper[: instruction["line"], :] | paper[: instruction["line"] : -1, :]
-else:
-    raise Exception("invalid direction", instruction["direction"])
+        paper = paper[:, : instruction["line"]] | paper[:, : instruction["line"] : -1]
+    case "y":
+        assert not np.any(paper[instruction["line"], :])
+        assert instruction["line"] == np.shape(paper)[0] // 2
+        paper = paper[: instruction["line"], :] | paper[: instruction["line"] : -1, :]
+    case _:
+        raise Exception("invalid direction", instruction["direction"])
 
 print("number of dots after the first fold: %i" % np.count_nonzero(paper))

16/part1.py
@@ -23,24 +23,25 @@ def parse_packets(message):
         packet["value"] = int(value, 2)
     else:  # operator packet
         length_type_id = message[6]
-        if length_type_id == "0":
-            HEADER_LENGTH = 3 + 3 + 1 + 15  # version, type ID, length type ID, length
-            total_subpacket_length = int(message[7:HEADER_LENGTH], 2)
-            packet["length"] = HEADER_LENGTH
-            while packet["length"] != HEADER_LENGTH + total_subpacket_length:
-                subpacket = parse_packets(message[packet["length"] :])
-                packet["subpackets"].append(subpacket)
-                packet["length"] += subpacket["length"]
-        elif length_type_id == "1":
-            HEADER_LENGTH = 3 + 3 + 1 + 11  # version type ID, length type ID, length
-            num_subpackets = int(message[7:HEADER_LENGTH], 2)
-            packet["length"] = HEADER_LENGTH
-            for s in range(num_subpackets):
-                subpacket = parse_packets(message[packet["length"] :])
-                packet["subpackets"].append(subpacket)
-                packet["length"] += subpacket["length"]
-        else:
-            raise Exception("unknown length type ID", length_type_id)
+        match length_type_id:
+            case "0":
+                HEADER_LENGTH = 3 + 3 + 1 + 15  # version, type ID, length type ID, length
+                total_subpacket_length = int(message[7:HEADER_LENGTH], 2)
+                packet["length"] = HEADER_LENGTH
+                while packet["length"] != HEADER_LENGTH + total_subpacket_length:
+                    subpacket = parse_packets(message[packet["length"] :])
+                    packet["subpackets"].append(subpacket)
+                    packet["length"] += subpacket["length"]
+            case "1":
+                HEADER_LENGTH = 3 + 3 + 1 + 11  # version type ID, length type ID, length
+                num_subpackets = int(message[7:HEADER_LENGTH], 2)
+                packet["length"] = HEADER_LENGTH
+                for s in range(num_subpackets):
+                    subpacket = parse_packets(message[packet["length"] :])
+                    packet["subpackets"].append(subpacket)
+                    packet["length"] += subpacket["length"]
+            case _:
+                raise Exception("unknown length type ID", length_type_id)
 
     return packet
 

commit eaa6ebce2c86b19f1e96365fc6bca4cc05432a03

Day 2, part 2: fix error introduced during refactoring
Fixes: 7d3afc34807e9d309cd7d4fd5f5fd8e880925e42 ("Use Python 3.10 match-case statements where applicable")

02/part2.py
@@ -18,7 +18,7 @@ for c in commands:
         case "down":
             aim += c["distance"]
         case "up":
-            c["distance"]
+            aim -= c["distance"]
         case _:
             raise Exception("invalid direction", c)
 